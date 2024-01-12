4e5f550dc60d16c905105a4017ee879348934fb5
nawadeveloper <nawarajusa@gmail.com>
2024-01-12 12:45:09 -0600

    animation added to processStep

--- client/src/components/designSets/ProcessStep.tsx
+++ client/src/components/designSets/ProcessStep.tsx
@@ -1,3 +1,5 @@
+import { motion } from "framer-motion";
+
 type processProps = {
   figure: string;
   detail: string;
@@ -5,10 +7,45 @@ type processProps = {
   mini_title: string;
   icon: string;
 };
+
+const parentVariations = {
+  initial: { opacity: 1 },
+  animate: {
+    opacity: 1,
+    transition: {
+      duration: 2,
+      staggerChildren: 0.5,
+      staggerDirection: 1,
+    },
+  },
+};
+
+const titleVariations = {
+  initial: { y: 100, opacity: 0 },
+  animate: {
+    y: 0,
+    opacity: 1,
+  },
+};
+
+const figureVariations = {
+  initial: { scale: 0 },
+  animate: { scale: 1 },
+};
+
 const ProcessStep = (props: processProps) => {
   return (
-    <div className="text-nawa-200 px-6 md:px-0">
-      <div className="flex gap-5 items-center mb-4">
+    <motion.div
+      variants={parentVariations}
+      initial="initial"
+      whileInView="animate"
+      viewport={{ once: true, amount: 0.8 }}
+      className="text-nawa-200 px-6 md:px-0"
+    >
+      <motion.div
+        variants={titleVariations}
+        className="flex gap-5 items-center mb-4"
+      >
         <div className="w-14 h-14 bg-nawa-100 rounded-full grid place-items-center">
           <span className="material-symbols-rounded text-nawa-400 text-3xl">
             {props.icon}
@@ -22,15 +59,20 @@ const ProcessStep = (props: processProps) => {
             {props.mini_title}
           </span>
         </div>
-      </div>
-      <p className="text-justify text-white">{props.detail}</p>
+      </motion.div>
+      <motion.p variants={titleVariations} className="text-justify text-white">
+        {props.detail}
+      </motion.p>
 
-      <div className="flex justify-center py-5">
+      <motion.div
+        variants={figureVariations}
+        className="flex justify-center py-5"
+      >
         <span className="material-symbols-rounded text-9xl">
           {props.figure}
         </span>
-      </div>
-    </div>
+      </motion.div>
+    </motion.div>
   );
 };
 
